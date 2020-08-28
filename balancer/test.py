15765173d31621d1a92b8f661b4c71b03a0d0d16
jawilk <hgu5tef4@gmail.com>
2020-08-28 23:19:01 +0200

    Adapt changes in balancer.py

--- balancer/test.py
+++ balancer/test.py
@@ -8,5 +8,5 @@ w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/'+keys.INFURA_ID))
 
 
 weth_bal = Pool(w3, '0x59A19D8c652FA0284f44113D0ff9aBa70bd46fB4')
-print(weth_bal.get_final_tokens())
-print(weth_bal.get_normalized_weight(weth_bal.get_final_tokens()[0]))
+print(weth_bal.final_tokens())
+print(weth_bal.get_normalized_weight(weth_bal.final_tokens()[0]))
