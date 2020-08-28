04512167a6dfe2836b7f236bd860d2c366f69470
jannis <jannis-wilkening@gmx.de>
2020-08-28 18:27:37 +0200

    1st Upload

---
+++ balancer/test.py
@@ -0,0 +1,12 @@
+from web3 import Web3
+
+from balancer import Pool
+import keys
+
+
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/'+keys.INFURA_ID))
+
+
+weth_bal = Pool(w3, '0x59A19D8c652FA0284f44113D0ff9aBa70bd46fB4')
+print(weth_bal.get_final_tokens())
+print(weth_bal.get_normalized_weight(weth_bal.get_final_tokens()[0]))
